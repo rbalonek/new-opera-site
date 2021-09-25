6da3210a8c38c026ccd6826075da48c5b72a53b9
Robert Balonek <robertbalonek@gmail.com>
2021-09-25 16:22:48 -0700

    Contact Fix Background

--- src/screens/Gallery/Gallery.jsx
+++ src/screens/Gallery/Gallery.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Gallery.css";
 
 export default function Gallery() {
   return (
-    <div>
+    <div className="gallery__container">
       <h1 style={{ color: "white" }}>GALLERY</h1>
     </div>
   );
