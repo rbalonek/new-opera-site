00323adf87d24d4e80d9cd5e2c6c718fdca25065
Robert Balonek <robertbalonek@gmail.com>
2021-09-25 20:41:45 -0700

    Media

---
+++ src/components/AudioPlayer/AudioPlayer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./AudioPlayer.css";
+
+import ReactAudioPlayer from "react-audio-player";
+
+export default function AudioPlayer(props) {
+  return (
+    <div className="audio-player__container">
+      <div className="audio-player__info">
+        <h2 className="audio-player__title">{props.title}</h2>
+        <p className="audio-player__role">Role: {props.role}</p>
+        <div className="audio-player__bottom">
+          <img
+            className="audio-player__img"
+            src={props.img}
+            alt={props.title}
+          />
+          <div className="audio-player__controls">
+            <ReactAudioPlayer className="player" src={props.URL} controls />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/screens/Media/Media.jsx
+++ src/screens/Media/Media.jsx
@@ -1,9 +1,77 @@
 import React from "react";
+import AudioPlayer from "../../components/AudioPlayer/AudioPlayer";
 import "./Media.css";
+
+import Saul from "../../components/Audio/Saul Sampler Shorter copy.mp3";
+import CarminaBurana from "../../components/Audio/Carmina Burana Sampler.mp3";
+import Shaunard from "../../components/Audio/Shaunard.mp3";
+import VoiceOverOne from "../../components/Audio/Reel - 5_25_20, 5.08 PM.mp3";
+import VoiceOverTwo from "../../components/Audio/60 sec reel2 2nd cut.mp3";
+
 export default function Media() {
   return (
-    <div>
-      <h1 style={{ color: "white" }}>MEDIA</h1>
+    <div className="media__container">
+      <div className="audio-video__split">
+        <div className="audio__section">
+          <h1>AUDIO</h1>
+          <div className="audio__players__container">
+            <AudioPlayer
+              title="Handel Saul excerpts"
+              role="Saul"
+              URL={Saul}
+              img="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1632620849/robertbalonek.com/BobLiveShots/Ottone_jrpexs.png"
+            />
+            <AudioPlayer
+              title="Carmina Burana excerpts"
+              role="Soloist"
+              URL={CarminaBurana}
+              img="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1632623733/robertbalonek.com/BobLiveShots/IMG_D0052F784D25-1_djgna7.jpg"
+            />
+            <AudioPlayer
+              title="La Boheme"
+              role="Shaunard"
+              URL={Shaunard}
+              img="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1601221847/robertbalonek.com/BobLiveShots/IMG_2592.JPG_pqvtap.jpg"
+            />
+            <h1>VOICE OVER</h1>
+            <AudioPlayer
+              title="Voice Over Reel"
+              role="Voice Over"
+              URL={VoiceOverOne}
+              img="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1601145832/robertbalonek.com/Headshots/CoverPg_jfbfqm.jpg"
+            />
+            <AudioPlayer
+              title="Voice Over Reel Two"
+              role="Voice Over"
+              URL={VoiceOverTwo}
+              img="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1632613515/robertbalonek.com/Headshots/hsFace_kbxsok.jpg"
+            />
+          </div>
+        </div>
+        <div className="video__section">
+          <h1>VIDEO</h1>
+          <iframe
+            className="media__video"
+            title="shaunard"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/SovJfO1mugQ"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          />
+          <iframe
+            className="media__video"
+            title="Largo"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/0c67QhA8B6o"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          />
+        </div>
+      </div>
     </div>
   );
 }
@@ -13,3 +81,5 @@ export default function Media() {
 //         src="https://bit.ly/3nWh78w"
 //         poster="posterImage.png"
 //       ></video>
+
+// https://www.dropbox.com/s/bbrriqyh5pxs8m2/Saul%20Sampler%20Shorter%20copy.mp3?dl=0
