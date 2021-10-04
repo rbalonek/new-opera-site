205d197437c7f087b8ab243dea1d0d3c0bd5104d
Robert Balonek <robertbalonek@gmail.com>
2021-10-04 13:34:17 -0700

    Links completed

--- src/components/UpcomingGigs/UpcomingGigs.jsx
+++ src/components/UpcomingGigs/UpcomingGigs.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import "./UpcomingGigs.css";
 export default function UpcomingGigs() {
+  const goToBarber = () => {
+    window.open("https://www.operaonthejames.org/shows");
+  };
+  const goToRig = () => {
+    window.open("https://www.bohemeopera.org/main-stage");
+  };
+  const goToMarcello = () => {
+    window.open("https://www.operamodesto.org/");
+  };
+  const goToErnesto = () => {
+    window.open("https://nycopera.com/shows/finzi/");
+  };
+
   return (
     <div className="upcoming__container">
       <h1>UPCOMING</h1>
       <h2 className="underline">2021</h2>
 
-      <div className="upcoming___show-container">
+      <div onClick={goToBarber} className="upcoming___show-container">
         <img
           alt="ootj"
           src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1624389679/robertbalonek.com/Performances/OOTJ%20Figaro/OOTJ_l2gjb0.webp"
@@ -23,7 +36,10 @@ export default function UpcomingGigs() {
       </div>
       <h2 className="underline">2022</h2>
       <div className="twentyTwo-shows">
-        <div className="upcoming___show-container split__2022">
+        <div
+          onClick={goToRig}
+          className="upcoming___show-container split__2022"
+        >
           <img
             alt="bohemeNJ"
             src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1601145907/robertbalonek.com/Logos/p3789966395-11_g5a6um.jpg"
@@ -39,7 +55,10 @@ export default function UpcomingGigs() {
           </p>
         </div>
 
-        <div className="upcoming___show-container split__2022">
+        <div
+          onClick={goToMarcello}
+          className="upcoming___show-container split__2022"
+        >
           <img
             alt="ootj"
             src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1624394360/robertbalonek.com/Logos/OperaModesto_tggc0y.jpg"
@@ -52,6 +71,24 @@ export default function UpcomingGigs() {
           </p>
           <p>
             DATE: <span className="bold">April 29- May 1st</span>
+          </p>
+        </div>
+        <div
+          onClick={goToErnesto}
+          className="upcoming___show-container split__2022"
+        >
+          <img
+            alt="NYCO"
+            src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1601215498/robertbalonek.com/Logos/NYCOpera_szn6kj.png"
+          />
+          <p>
+            OPERA: <span className="bold">Garden of Finzi-Continis</span>
+          </p>
+          <p>
+            ROLE: <span className="bold">Ernesto</span>
+          </p>
+          <p>
+            DATE: <span className="bold">Jan 19- 30th</span>
           </p>
         </div>
       </div>
