e6aa067089634063e444491916a5f21cc5093cbc
Robert Balonek <robertbalonek@gmail.com>
2021-10-28 20:43:06 -0400

    Cell Adjust for Banner

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router";
 
 import About from "../screens/About/About";
 import Engagements from "../screens/Engagements/Engagements";
-import Home from "../screens/Home/Home";
+// import Home from "../screens/Home/Home";
 import Media from "../screens/Media/Media";
 import Gallery from "../screens/Gallery/Gallery";
 import Contact from "../screens/Contact/Contact";
