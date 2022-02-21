348ac886c1489c26d4369c2f8aee4b639d637785
Robert Balonek <robertbalonek@gmail.com>
2022-02-21 10:44:43 -0800

    Meta Pixel

--- src/App.js
+++ src/App.js
@@ -10,8 +10,30 @@ import {
   Event,
 } from "./Tracking";
 
+import ReactPixel from "react-facebook-pixel";
+
+// const advancedMatching = { em: 'some@email.com' }; // optional, more info: https://developers.facebook.com/docs/facebook-pixel/advanced/advanced-matching
+const options = {
+  autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
+  debug: false, // enable logs
+};
+ReactPixel.init(
+  process.env.REACT_APP_META_CODE,
+  // advancedMatching,
+  options
+);
+
+// ReactPixel.pageView(); // For tracking page view
+// ReactPixel.track(event, data); // For tracking default events. More info about standard events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#standard-events
+// ReactPixel.trackSingle('PixelId', event, data); // For tracking default events.
+// ReactPixel.trackCustom(event, data); // For tracking custom events. More info about custom events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#custom-events
+// ReactPixel.trackSingleCustom('PixelId', event, data); // For tracking custom events.
+
 function App() {
+  // console.log("META", process.env.REACT_APP_META_CODE);
+  // console.log("GOOGLE", process.env.REACT_APP_THING);
   initGA(process.env.REACT_APP_THING);
+  ReactPixel.pageView();
   // PageView();
   return (
     <Layout Event={Event}>
