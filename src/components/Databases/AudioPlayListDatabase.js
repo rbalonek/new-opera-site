4428c4ec45679b3f67587f7addb20db3dedb7c0e
Robert Balonek <robertbalonek@gmail.com>
2022-05-29 19:11:30 -0700

    New Media

--- src/components/Databases/AudioPlayListDatabase.js
+++ src/components/Databases/AudioPlayListDatabase.js
@@ -1,23 +1,23 @@
-// import Saul from "../../components/Audio/Saul Sampler Shorter copy.mp3"
-// import CarminaBurana from "../../components/Audio/Carmina Burana Sampler.mp3";
-// import Shaunard from "../../components/Audio/Shaunard.mp3";
-// import Haydn from "../../components/Audio/Haydn Creation Sample.mp3";
-// import VoiceOverOne from "../../components/Audio/Reel - 5_25_20, 5.08 PM.mp3";
-// import VoiceOverTwo from "../../components/Audio/60 sec reel2 2nd cut.mp3";
+import Saul from "../../components/Audio/Saul_Sampler_Shorter.mp3"
+import CarminaBurana from "../../components/Audio/Carmina_Burana_Sampler.mp3";
+import Shaunard from "../../components/Audio/Shaunard.mp3";
+import Haydn from "../../components/Audio/Haydn_Creation_Sample.mp3";
+import VoiceOverOne from "../../components/Audio/VO_Reel.mp3";
+import VoiceOverTwo from "../../components/Audio/60_sec_2nd_cut.mp3";
 
 const AudioPlayList = [
   {
   title: "Handel Saul excerpts",
     role: "Saul",
     // URL: { Saul },
-    URL: "/static/media/Saul Sampler Shorter copy.8ed67463.mp3",
-  // URL: "/static/media/Saul Sampler Shorter copy.8ed67463.mp3",
-  img: "https://res.cloudinary.com/bobalobbadingdong/image/upload/v1632620849/robertbalonek.com/BobLiveShots/Ottone_jrpexs.png",
+    URL: "/static/media/Saul_Sampler_Shorter.8ed67463.mp3",
+    url_helper:"Saul",
+    img: "https://res.cloudinary.com/bobalobbadingdong/image/upload/v1632620849/robertbalonek.com/BobLiveShots/Ottone_jrpexs.png",
   },
   {
     title: "Carmina Burana excerpts",
     role: "Soloist",
-    URL: "/static/media/Carmina Burana Sampler.207a2b1e.mp3",
+    URL: "/static/media/Carmina_Burana_Sampler.207a2b1e.mp3",
     img: "https://res.cloudinary.com/bobalobbadingdong/image/upload/v1653865853/robertbalonek.com/BobLiveShots/IMG_D0052F784D25-1_djgna7_lmmamb.png",
   },
   {
@@ -29,19 +29,19 @@ const AudioPlayList = [
   {
     title: "Haydn Creation",
     role: "Soloist",
-    URL: "/static/media/Haydn Creation Sample.2528dced.mp3",
+    URL: "/static/media/Haydn_Creation_Sample.2528dced.mp3",
     img: "https://res.cloudinary.com/bobalobbadingdong/image/upload/v1601145841/robertbalonek.com/Headshots/Too_Much-_ay3b0k.jpg",
   },
   {
     title: "Voice Over Reel",
     role: "Voice Over",
-    URL: "/static/media/Reel - 5_25_20, 5.08 PM.044e82c6.mp3",
+    URL: "/static/media/VO_Reel.044e82c6.mp3",
     img: "https://res.cloudinary.com/bobalobbadingdong/image/upload/v1601145832/robertbalonek.com/Headshots/CoverPg_jfbfqm.jpg",
   },
   {
     title: "Voice Over Reel Two",
     role: "Voice Over",
-    URL: "/static/media/60 sec reel2 2nd cut.b0db36e4.mp3",
+    URL: "/static/media/60_sec_2nd_cut.b0db36e4.mp3",
     img: "https://res.cloudinary.com/bobalobbadingdong/image/upload/v1632613515/robertbalonek.com/Headshots/hsFace_kbxsok.jpg",
     },
 ]
