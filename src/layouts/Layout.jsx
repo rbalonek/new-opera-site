03c4aee8ab7fa6a3542ddf38de580c3213ba4907
Robert Balonek <robertbalonek@gmail.com>
2022-05-29 21:36:51 -0700

    Continual Play

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -16,14 +16,16 @@ export default function Layout(props) {
   const [toggle, setToggle] = useState(false)
   const [button, setButton] = useState(PlayList)
   const [buttonText, setButtonText] = useState("Show Player")
-  const [modalIsOpen, setIsOpen] = useState(false);
+  // const [modalIsOpen, setIsOpen] = useState(false);
+  const [showAudioPlayer, setShowAudioPlayer]=useState("hide")
 
   const toggleClick = () => {
     if (toggle === false) {
       setToggle(true)
       setButton(Stop)
       setButtonText("Hide Player")
-      openModal()
+      setShowAudioPlayer("show")
+      // openModal()
       if (window.pageYOffset < 3) {
         window.scroll(0, 100)
       }
@@ -31,25 +33,26 @@ export default function Layout(props) {
       setToggle(false)
       setButton(PlayList)
       setButtonText("Show Player")
-      closeModal()
+      setShowAudioPlayer("hide")
+      // closeModal()
     }
   }
 
-  function openModal() {
+  // function openModal() {
     
 
-    setIsOpen(true);
-    var x = window.scrollX;
-    var y = window.scrollY;
-    window.onscroll = function () {
-      window.scrollTo(x, y);
-    };
-  }
+  //   setIsOpen(true);
+  //   var x = window.scrollX;
+  //   var y = window.scrollY;
+  //   window.onscroll = function () {
+  //     window.scrollTo(x, y);
+  //   };
+  // }
 
-  function closeModal() {
-    setIsOpen(false);
-    window.onscroll = function () {};
-  }
+  // function closeModal() {
+  //   setIsOpen(false);
+  //   window.onscroll = function () {};
+  // }
   
   return (
     <div>
@@ -57,18 +60,22 @@ export default function Layout(props) {
       <main>{props.children}</main>
       <ListenButton button={button} buttonText={buttonText} toggle={toggle} toggleClick={toggleClick} />
       
-      <Modal
-      isOpen={modalIsOpen}
-      // onAfterOpen={afterOpenModal}
-      onRequestClose={closeModal}
-      // style={customStyles}
-      className="Modal flexy"
-        contentLabel="Modal"
-        ariaHideApp={false}
-    >
-    <NewAudioPlayer toggle={toggle} toggleClick={toggleClick} songs={AudioPlayList} />
-    </Modal>
+      <div className={showAudioPlayer}>
+      <NewAudioPlayer toggle={toggle} toggleClick={toggleClick} songs={AudioPlayList} />
+      </div>
+    
     <Footer />
     </div>
   );
 }
+
+// <Modal
+// isOpen={modalIsOpen}
+// // onAfterOpen={afterOpenModal}
+// onRequestClose={closeModal}
+// // style={customStyles}
+// className="Modal flexy"
+//   contentLabel="Modal"
+//   ariaHideApp={false}
+// >
+// </Modal>
